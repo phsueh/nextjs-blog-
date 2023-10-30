3133c46d686ea52d2bfd8191d229d41198f2a5ce
Paul Hsueh <hsueh.paul@gmail.com>
2023-10-30 14:45:32 -0400

    made more changes

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home( {allPostsData} ) {
       </Head>
       
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Paul. A software engineering. I built this Next.js</p>
+        <p>Hello, I'm Paul. A software engineering. I built this using Next.js</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
